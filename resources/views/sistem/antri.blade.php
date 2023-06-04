56e5732cb0f41d21781e5f27c29f5b379977d375
Muhammad Hemas Julang Arrasy <91818681+hemasja@users.noreply.github.com>
2023-06-04 23:11:49 +0700

    UPDATE: Making Admin Page

---
+++ resources/views/sistem/antri.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Admin Page</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="/resources/" />
+  </head>
+  <body>
+    <form action="" method="post">
+        <div class="card">
+          <div class="card-body" style="background-color: #e3f2fd">
+            <h3 class="card-title">Admin Page</h3>
+            <button type="button" class="btn btn-danger mt-lg-4">Reset</button>
+            <button type="button" class="btn btn-info mt-lg-4 ms-xxl-4">Logout</button>
+          </div>
+        </div>
+    
+        <div class="card mt-lg-5">
+          <div class="card-body" style="background-color: #e3f2fd">
+            <h3 class="card-title">List Antrian</h3>
+            <table class="table">
+              <thead>
+                <tr class="table-info">
+                  <th scope="col">No</th>
+                  <th scope="col">NIK</th>
+                  <th scope="col">Nama Lengkap</th>
+                  <th scope="col">Email</th>
+                  <th scope="col">No HP</th>
+                  <th scope="col">Keperluan</th>
+                  <th scope="col">Status</th>
+                  <th scope="col">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>11321313123</td>
+                  <td>adkaowkwodoka</td>
+                  <td>adakwdwok@gmail.com</td>
+                  <td>10391308123</td>
+                  <td>ngentot</td>
+                  <td>Dilayani</td>
+                  <td><button type="button" class="btn btn-warning me-sm-5">Update</button><button type="button" class="btn btn-danger">Delete</button></td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>11321313123</td>
+                  <td>adkaowkwodoka</td>
+                  <td>adakwdwok@gmail.com</td>
+                  <td>10391308123</td>
+                  <td>ngentot</td>
+                  <td>Dilayani</td>
+                  <td><button type="button" class="btn btn-warning me-sm-5">Update</button><button type="button" class="btn btn-danger">Delete</button></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+    </form>
+  </body>
+</html>
